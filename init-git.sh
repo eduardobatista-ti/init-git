59987520ab806daa7df68bf2f420d15f36159b4b
Eduardo Batista <50236895+eduardobatista-ti@users.noreply.github.com>
2024-08-06 12:53:11 -0300

    feat: add update branches with stash

--- init-git.sh
+++ init-git.sh
@@ -20,11 +20,47 @@ branch_exists() {
   git show-ref --verify --quiet refs/heads/"$1"
 }
 
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-echo "############ rep: eduardobatista-ti/init-git ############"; echo "Script de atulização de repositórios e criação de Branch."; progress_bar;
+echo "############ rep: eduardobatista-ti/init-git ############"; echo "Script de atualização de repositórios e criação de Branch."; progress_bar;
 
 PROJECT_DIR=$(pwd)
+
+read -r -p "Desejea atualizar Branches sem criar uma nova? (s/n): " apply_stash
+
+if [ "$apply_stash" = "s" ]; then
+  echo "Atualizando Branches..."; progress_bar
+  git stash
+  git fetch
+        if branch_exists "main"; then
+            echo "Atualizando 'main'"; progress_bar
+            git checkout main && git pull || { echo "Falha ao mudar para a branch 'main' ou ao realizar o pull."; exit 1; }  
+          else
+            echo "Branch 'main' não existe. Saindo da execução."
+            exit 1
+        fi
+        if branch_exists "develop"; then
+            echo "Atualizando 'develop'"; progress_bar
+           git checkout develop && git pull || { echo "Falha ao mudar para a branch 'develop' ou ao realizar o pull."; exit 1; }
+          else
+            echo "Branch 'develop' não existe. Saindo da execução."
+            exit 1
+        fi
+   git checkout "$CURRENT_BRANCH"
+   git merge develop 
+   git stash apply
+   echo "Atualização completa. Saindo da execução."
+   exit 0   
+
+else
+  echo "Partindo para processo de criação e atualização."
+  progress_bar
+fi
+
+
 read -r -p "Você vai sair da branch atual e pode perder alterações não salvas. Deseja sair da branch atual? (s/n): " leave_branch
+
+
 
 if [ "$leave_branch" = "s" ]; then
   echo "Saindo da branch atual..."; progress_bar
@@ -64,5 +100,6 @@ if [ "$create_branch" = "s" ]; then
   fi
 else
   echo "Não foi criada uma nova branch! Saindo da execução."
+  git checkout "$CURRENT_BRANCH" 
 fi
 
